523263a4a0433ab6b433c5046b086b2ea753d68d
DarcioRafael <liminhadarcioo@gmail.com>
2024-10-25 18:46:47 -0300

    Add files via upload

---
+++ exercicioaula6.py
@@ -0,0 +1,20 @@
+# exercicio = int(input(' digite qual exercicio a execukltar: 1 ou 2 '))
+
+# if exercicio == 1:
+
+#     nome = input(' digite o seu nome ')
+#     sobrenome = input(' digite o sobrenome ')
+#     idade = input(' digite a idade ')
+
+#     listaA = [ nome, sobrenome, idade]
+#     print(listaA[0], listaA[1],', idade', listaA[2], 'anos')
+
+# elif exercicio == 2:
+
+#     mes =int(input(' digite um més de 1 a 12 '))
+#     mes=mes-1
+#     listaA= [ 'janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho', 'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro ']
+#     print (' o més é :',listaA[mes])
+# else:
+#     print(' digito invalido')
+
